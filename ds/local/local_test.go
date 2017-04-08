f21ca48a03d03f9c39eaa6ad6f62a07b0bc83e85
Evan Hazlett <ejhazlett@gmail.com>
2017-04-07 22:10:00 -0400

    fix tests; remove unneeded ipam pkg
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- ds/local/local_test.go
+++ ds/local/local_test.go
@@ -5,7 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/ehazlett/circuit/config"
+	"github.com/containernetworking/cni/libcni"
+	"github.com/containernetworking/cni/pkg/types"
 )
 
 func getLocalDS() (*localDS, error) {
@@ -14,9 +15,7 @@ func getLocalDS() (*localDS, error) {
 		return nil, err
 	}
 
-	return &localDS{
-		statePath: d,
-	}, nil
+	return NewLocalDS(d)
 }
 
 func TestLocalSaveNetwork(t *testing.T) {
@@ -26,67 +25,22 @@ func TestLocalSaveNetwork(t *testing.T) {
 	}
 	defer os.Remove(l.statePath)
 
-	n := &config.Network{
-		Name:   "testing",
-		Subnet: "10.254.0.0",
+	n := &libcni.NetworkConfig{
+		Network: &types.NetConf{
+			Name: "testing",
+		},
 	}
 
 	if err := l.SaveNetwork(n); err != nil {
 		t.Fatal(err)
 	}
 
-	network, err := l.GetNetwork(n.Name)
+	network, err := l.GetNetwork(n.Network.Name)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if network.Name != n.Name {
-		t.Fatalf("expected network name %q; received %q", n.Name, network.Name)
-	}
-}
-
-func TestLocalSaveIPAddr(t *testing.T) {
-	l, err := getLocalDS()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(l.statePath)
-
-	n := &config.Network{
-		Name:           "testing",
-		Subnet:         "10.254.0.0",
-		BandwidthBytes: 2048,
-	}
-
-	if err := l.SaveNetwork(n); err != nil {
-		t.Fatal(err)
-	}
-
-	network, err := l.GetNetwork(n.Name)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if network.Name != n.Name {
-		t.Fatalf("expected network name %q; received %q", n.Name, network.Name)
-	}
-
-	testIP := "10.254.0.1"
-
-	if err := l.SaveIPAddr(testIP, network.Name); err != nil {
-		t.Fatal(err)
-	}
-
-	ips, err := l.GetNetworkIPs(network.Name)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(ips) != 1 {
-		t.Fatalf("expected 1 network; received %d", len(ips))
-	}
-
-	if ips[0].String() != testIP {
-		t.Fatalf("expected ip %q; received %q", testIP, ips[0].String())
+	if network.Network.Name != n.Network.Name {
+		t.Fatalf("expected network name %q; received %q", n.Network.Name, network.Network.Name)
 	}
 }
